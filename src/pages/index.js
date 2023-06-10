4ac559d8ed2ef609609529eff42a2cd748c51387
vikas <vikas.aurasoft@gmail.com>
2023-06-10 09:01:53 +0530

    starting step form

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Test from "./test";
 import NewTest from "./newTest";
 import Style from '../styles/school.module.css'
 import Oldform from "./newTest";
+import UpdateFromStudent from "@/Component/teacher/update-student-form";
 
 const index = () => {
   return (
@@ -19,10 +20,12 @@ const index = () => {
 
 {/* <AllUser /> */}
 
+<UpdateFromStudent />
+
 <div className={Style.MainImg}>
 
 
-<img src="https://repository-images.githubusercontent.com/320789182/05666c80-6fbe-11eb-8159-9c143259a9aa" alt="" />
+{/* <img src="https://repository-images.githubusercontent.com/320789182/05666c80-6fbe-11eb-8159-9c143259a9aa" alt="" /> */}
 
 </div>
 
---
+++ src/pages/loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/common.module.css'
+const loading = () => {
+  return (
+    <>
+      <section className={styles.loading_section}>
+            <div className={styles.loading}>
+            </div>
+        </section> 
+    </>
+  )
+}
+
+export default loading
